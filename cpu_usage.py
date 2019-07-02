eae4ed7cea47cfa721116fdc672e49993b5fc5d5
Justin Embree <jembree18@gmail.com>
2019-07-01 23:04:56 -0400

    made return values more standard, variable clarity, added comments

--- cpu_usage.py
+++ cpu_usage.py
@@ -1,96 +1,81 @@
-"Functions for reading proc/stat, and finding total and core cpu usage"
+"""Functions for reading proc/stat, and finding total and individual core cpu usage"""
 
 # required imports
 
 import time
-import json
-
-# Read in proc/stat, creating initial line search variable (probably should call per function)
-
-stat_file = open('/proc/stat', 'r')
-
-#line_number = stat_file.readlines()
-
 
 # finds line for total cpu, always line 0
 
-core_dict = {}
 
-
-def total_cpu():
+def __total_cpu() -> (int, int, int, int, int):
+    """
+    returns usage for all cores
+    :return: tuple of user, nice, system, idle, iowait
+    """
     stat_file = open('/proc/stat', 'r')
     line_number = stat_file.readlines()
     total_cpu_line = line_number[0].split()
     user, nice, system, idle, iowait = (total_cpu_line[1], total_cpu_line[2], total_cpu_line[3], total_cpu_line[4],
                                         total_cpu_line[5])
-    return user, nice, system, idle, iowait
+    return int(user), int(nice), int(system), int(idle), int(iowait)
 
 
-# searches through stat to find the correct core, returns processes executed
-
-
-
-def cpu_core(n):
+def __cpu_core(n: int) -> (str, int, int, int, int, int):
+    """
+    returns info for n core
+    :param n: 0 indexed core to check
+    :return: tuple of cpu name, user ticks, nice ticks, system ticks, idle ticks, iowait ticks
+    """
     stat_file = open('/proc/stat', 'r')
     line_number = stat_file.readlines()
-    try:
-        cpu_check = 0
-        while line_number[cpu_check].find('cpu%s' % n) != 0:  # checking lines for correct cpu
-            cpu_check += 1
-        else:                                                   # returning processes if cpu'n' exists
-            cpu_line = line_number[cpu_check].split()
-            cpu, user, nice, system, idle, iowait = (cpu_line[0], (cpu_line[1]), cpu_line[2], cpu_line[3],
-                                                          cpu_line[4], cpu_line[5])
-            global core_dict
-            # core_dict = {"cpu": cpu, 'user': user, 'nice': nice, 'system': system, 'idle': idle, 'iowait': iowait}
-            return cpu, user, nice, system, idle, iowait
-            # return core_dict
-    except IndexError:                                      # catch if requesting core that does not exist
-        return "There is no core %s on this system!" % n
-    except:                                                 # other catch so function completes
-        return "Something went wrong!"
+    cpu_check = 0
+    while line_number[cpu_check].find('cpu%s' % n) != 0:  # checking lines for correct cpu
+        cpu_check += 1
+    else:                                                   # returning processes if cpu'n' exists
+        cpu_line = line_number[cpu_check].split()
+        cpu, user, nice, system, idle, iowait = (cpu_line[0], (cpu_line[1]), cpu_line[2], cpu_line[3],
+                                                 cpu_line[4], cpu_line[5])
+        return cpu, int(user), int(nice), int(system), int(idle), int(iowait)
 
 
-def total_cpu_usage():
-    user0, nice0, system0, idle0, iowait0 = total_cpu()
-    total_used_processes0 = int(user0) + int(nice0) + int(system0)
-    total_idle0 = int(idle0)
+def total_cpu_usage() -> float:
+    """
+    calculates core usage based on active and idle ticks
+    :return: value within range of 0 - 100
+    """
+    user_initial, nice_initial, system_initial, idle_initial, iowait_initial = __total_cpu()
+    total_active_processes_initial = user_initial + nice_initial + system_initial
+    total_idle_initial = idle_initial + iowait_initial
 
     time.sleep(1)
 
-    user1, nice1, system1, idle1, iowait1 = total_cpu()
-    total_used_processes1 = int(user1) + int(nice1) + int(system1)
-    total_idle1 = int(idle1)
-    used_process_diff = total_used_processes1 - total_used_processes0
-    idle_diff = total_idle1 - total_idle0
-    total_percentage = used_process_diff / idle_diff * 100     #TODO: NEEDS TO BE TOTAL USED OVER TOTAL (USED PLUS IDLE)
-    return "{}%".format(total_percentage)
+    user_final, nice_final, system_final, idle_final, iowait_final = __total_cpu()
+    total_active_processes_final = user_final + nice_final + system_final
+    total_idle_final = idle_final + iowait_final
+
+    active_diff = total_active_processes_final - total_active_processes_initial
+    idle_diff = total_idle_final - total_idle_initial
+    total_percentage = active_diff / (active_diff + idle_diff) * 100
+    return total_percentage
 
 
-def cpu_core_usage(n):
-    try:
-        cpu0, user0, nice0, system0, idle0, iowait0 = cpu_core(n)
-        total_used_processes0 = int(user0) + int(nice0) + int(system0)
-        total_idle0 = int(idle0)
-        #core_dict0 = cpu_core(n)
-        #total_used_dict = core_dict0['user']
-        #print(total_used_dict)
+def cpu_core_usage(n: int) -> float:          # cpu does not change, same variable for initial/ final
+    """
+    returns per core usage based on active and idle ticks
+    :param n: 0 indexed core to check
+    :return:
+    """
+    cpu, user_initial, nice_initial, system_initial, idle_initial, iowait_initial = __cpu_core(n)
+    total_active_processes_initial = user_initial + nice_initial + system_initial
+    total_idle_initial = idle_initial + iowait_initial
 
-        time.sleep(1)
+    time.sleep(1)
 
-        cpu1, user1, nice1, system1, idle1, iowait1 = cpu_core(n)
-        total_used_processes1 = int(user1) + int(nice1) + int(system1)
-        total_idle1 = int(idle1)
-        used_process_diff = total_used_processes1 - total_used_processes0
-        idle_diff = total_idle1 - total_idle0
-        core_percentage = used_process_diff / idle_diff * 100
-        return "{}%".format(core_percentage)
-    except ValueError:
-        return "There is no core %s on this system!" % n
+    cpu, user_final, nice_final, system_final, idle_final, iowait_final = __cpu_core(n)
+    total_active_processes_final = user_final + nice_final  + system_final
+    total_idle_final = idle_final + iowait_final
 
-
-print(" single core", cpu_core_usage(5))
-
-core_json = json.dumps(core_dict)
-
-print(core_json)
+    active_diff = total_active_processes_final - total_active_processes_initial
+    idle_diff = total_idle_final - total_idle_initial
+    core_percentage = active_diff / (active_diff + idle_diff) * 100
+    return core_percentage
